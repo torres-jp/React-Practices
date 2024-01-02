7c11a89f1be56fa2dcf9eb01fd11c023a0d686dc
unknown <torres.jpw@gmail.com>
2024-01-02 12:25:43 -0300

    calendar proyect end

--- react-bigcalendar/src/App.jsx
+++ react-bigcalendar/src/App.jsx
@@ -1,6 +1,11 @@
 import { Calendar, dayjsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import dayjs from "dayjs";
+import { CiCalendarDate } from "react-icons/ci";
+import "dayjs/locale/es";
+import "./App.css";
+
+dayjs.locale("es");
 
 function App() {
   const localizar = dayjsLocalizer(dayjs);
@@ -10,18 +15,53 @@ function App() {
       start: dayjs("2024-01-10T09:00:00").toDate(),
       end: dayjs("2024-01-10T10:00:00").toDate(),
       title: "Evento 1",
+      data: {
+        x: 20,
+      },
     },
     {
       start: dayjs("2024-01-01T09:00:00").toDate(),
       end: dayjs("2024-01-02T15:00:00").toDate(),
       title: "Evento 1",
+      data: {
+        x: 10,
+      },
     },
   ];
 
   const components = {
     event: (props) => {
-      console.log(props);
-      return <div>Test</div>;
+      const { data } = props.event;
+
+      if (data.x > 15) {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              gap: "5px",
+              background: "red",
+            }}
+          >
+            <CiCalendarDate />
+            {props.title}
+          </div>
+        );
+      } else {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              gap: "5px",
+              background: "green",
+            }}
+          >
+            <CiCalendarDate />
+            {props.title}
+          </div>
+        );
+      }
     },
   };
 
