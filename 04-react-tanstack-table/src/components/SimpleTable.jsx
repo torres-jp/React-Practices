19ccbb0cf1bf459a82fb5aa7ec440a5b3dea2749
pnauj <torres.jpw@gmail.com>
2024-01-02 22:07:09 -0300

    start with the table

--- 04-react-tanstack-table/src/components/SimpleTable.jsx
+++ 04-react-tanstack-table/src/components/SimpleTable.jsx
@@ -1,5 +1,63 @@
+import { useReactTable } from "@tanstack/react-table";
+import data from "../assets/MOCK_DATA.json";
+
 function simpleTable() {
-  return <div>simpleTable</div>;
+  const columns = [
+    {
+      header: "ID",
+      accessorKey: "id",
+    },
+    {
+      header: "Name",
+      accessorKey: "name",
+    },
+    {
+      header: "Lastname",
+      accessorKey: "lastname",
+    },
+    {
+      header: "Email",
+      accessorKey: "email",
+    },
+    {
+      header: "Country",
+      accessorKey: "country",
+    },
+    {
+      header: "Day of Birth",
+      accessorKey: "dayOfBirth",
+    },
+  ];
+
+  const table = useReactTable({ data, columns });
+
+  return (
+    <div>
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>{header.column.columnDef.header}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>1</td>
+          </tr>
+        </tbody>
+
+        <tfoot>
+          <tr>
+            <td>id</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
 }
 
 export default simpleTable;
