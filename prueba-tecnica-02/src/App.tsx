d3d7ac1d3c36e9756accbbb6180481b48b730ce7
torres-jp <torres.jpw@gmail.com>
2025-05-04 17:06:27 -0300

    feat: implement movie fetching and display functionality

--- prueba-tecnica-02/src/App.tsx
+++ prueba-tecnica-02/src/App.tsx
@@ -1,8 +1,41 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+export interface Movie {
+  adult: boolean
+  backdrop_path: string
+  genre_ids: number[]
+  id: number
+  origin_country: string[]
+  original_language: string
+  original_name: string
+  overview: string
+  popularity: number
+  poster_path: string
+  first_air_date: Date
+  name: string
+  vote_average: number
+  vote_count: number
+}
+
+function getRandomMoive(movies: Movie[]): Movie {
+  return movies[Math.floor(Math.random() * movies.length)]
+}
+
+function getPartialMovieName(movie: Movie): string {
+  const indexes = Array.from({ length: movie.name.length }, (_, index) => index)
+    .sort(() => (Math.random() >= 0.5 ? 1 : -1))
+    .slice(0, Math.floor(movie.name.length / 2))
+
+  return movie.name.split('').reduce((name, letter, index) => {
+    if (indexes.includes(index)) name = name.concat(letter)
+
+    return name
+  }, '')
+}
+
 function App() {
-  const [data, setData] = useState<any[]>([])
+  const [movie, setMovie] = useState<Movie | null>(null)
 
   useEffect(() => {
     fetch('https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1 ', {
@@ -10,12 +43,17 @@ function App() {
         Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
       },
     })
-      .then((res) => res.json())
-      .then(setData)
+      .then((res) => res.json() as Promise<{ results: Movie[] }>)
+      .then((data) => setMovie(getRandomMoive(data.results)))
   }, [])
 
-  return <div>{JSON.stringify(data, null, 2)}</div>
+  if (!movie) {
+    return <div>Loading...</div>
+  }
+
+  console.log(getPartialMovieName(movie))
+
+  return <main>{movie.name}</main>
 }
 
 export default App
-//https://developer.themoviedb.org/reference/tv-series-top-rated-list
